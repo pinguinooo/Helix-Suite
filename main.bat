9de9d873e294f4634cf242bd0a4628644d1677dd
itsbigboi <b16uyz@gmail.com>
2022-05-03 17:45:35 -0400

    h

--- main.bat
+++ main.bat
@@ -11,8 +11,9 @@ echo ===========================================================================
 pause >nul
 cls
 :top
+set version=1.0.0
 echo                                          Welcome to the Helix Suite
-echo                                                Version 1.0.0
+echo                                               Version : %Version%
 pause
 cd %programfiles%
 if exist HelixTools (goto start) else (md HelixTools)
@@ -22,12 +23,12 @@ echo 1.Helix App Installer
 echo 2.Semi Windows Repair (requires restarts)
 echo 3.Full Windows Repair - Not Automated (requires restarts)
 echo 4.Ip Lookup
-echo 5.
+echo 5.Tron
 echo 6.
 echo 7.
 echo 8.
 echo 9.
-ECHO 0.
+ECHO 0.Credits
 set choice=
 set /p choice=Type the number to pick a preset : 
 if not '%choice%'=='' set choice=%choice:~0,1%
@@ -35,7 +36,7 @@ if '%choice%'=='1' goto Helix
 if '%choice%'=='2' goto Windowsrepair
 if '%choice%'=='3' goto windowsrepairfull
 if '%choice%'=='4' goto iplookupstart
-if '%choice%'=='5' goto 
+if '%choice%'=='5' goto tron
 if '%choice%'=='6' goto 
 if '%choice%'=='7' goto 
 if '%choice%'=='8' goto 
@@ -49,6 +50,9 @@ goto top
 
 :Helix
 cls
+cd %programfiles%/HelixTools
+echo not done
+pause
 goto top
 
 :Windowsrepair
@@ -181,3 +185,24 @@ del "%temp%\%webclient%.vbs" /f /q /s >nul
 del "%temp%\response.txt" /f /q /s >nul
 pause
 if '%ip%'=='' goto menu
+
+
+:Tron
+cls
+color 4
+echo PLEASE READ THIS
+echo 7
+echo I am not liable for anything that happens to your system with this script.
+echo You may continue in 10 seconds
+timeout 10
+pause
+
+:Credits
+cls
+
+echo 		+======================================+
+echo 		+			  Helix Credits			   +
+echo 		+ I do not take any credit for tron.   +
+echo 		+ You can download it here.            +
+echo 		+ Tron: https://github.com/bmrf/tron   +
+echo 		+======================================+
